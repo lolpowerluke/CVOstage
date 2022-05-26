e62ca3a3603747223a2dd96c7a8c0bd63a6bca8e
lolpowerluke <lukas.van.heddegem@gmail.com>
2022-05-26 17:25:26 +0200

    automatically sort the tools list by name

--- functions.js
+++ functions.js
@@ -10,6 +10,25 @@ function checkDarkLightCookie() {
     document.cookie="dark; expires=" + tomorrow + "; path=/";
     document.getElementById("body").className = "dark";
   }
+  var list, i, switching, b, shouldSwitch;
+  list = document.getElementById("tools-list");
+  switching = true;
+  while (switching) {
+    switching = false;
+    b = list.getElementsByTagName("button");
+    for (i = 0; i < (b.length - 1); i++) {
+      shouldSwitch = false;
+      if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
+  }
+
 }
 function darklight() {
   if (document.getElementById("body").classList.contains('dark')) {
